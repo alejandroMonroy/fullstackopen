182a737686a62b742bdc1813cf05fde351e3ca4c
Alejandro Monroy <alexmonroym91@gmail.com>
2024-09-09 11:56:38 +0200

    Exercise 2.3

--- part2/courseinfo/src/Total.jsx
+++ part2/courseinfo/src/Total.jsx
@@ -2,7 +2,7 @@ const Total = (props) => {
     console.log(props)
     return(
         <>
-            <b>total of {props.parts.reduce((n, {exercises}) => n + exercises, 0)} exercises</b>
+            <b>total of {props.parts.reduce((totalExercises, {exercises}) => totalExercises + exercises, 0)} exercises</b>
         </>
     )
 }
