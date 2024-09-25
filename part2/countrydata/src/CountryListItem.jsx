1274425d077df984f3bc25ed57a3951efa8609fc
Alejandro Monroy <alexmonroym91@gmail.com>
2024-09-25 11:18:55 +0200

    Exercise 2.19

--- part2/countrydata/src/CountryListItem.jsx
+++ part2/countrydata/src/CountryListItem.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
+import Country from './Country'
+
 const CountryListItem = ({name}) => {
+    const [showCountryDetail, setShowCountryDetail ] = useState(false)
+
+    const handleShowCountryDetail = () => {
+        setShowCountryDetail(!showCountryDetail)
+    }
+
     return(
-        <h1>{name}</h1>
+        <>
+            {showCountryDetail ? <Country name={name} /> : <h1>{name}</h1>}
+            <button onClick={handleShowCountryDetail}>{showCountryDetail ? 'hide' : 'show'}</button>
+        </>        
     )
 }
 
