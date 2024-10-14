c5f2965ecc77b460571297bfbfe9b3a810a6246b
Alejandro Monroy <alexmonroym91@gmail.com>
2024-10-14 16:49:52 +0200

    Update persons.js

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://fullstackopen-r6ys.onrender.com/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(response => response.data)
 
