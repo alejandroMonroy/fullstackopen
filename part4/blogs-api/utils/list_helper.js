c296234d5ee0f4933ce91d5a6db7c9aa95715282
Alejandro Monroy <alexmonroym91@gmail.com>
2024-10-25 13:42:10 +0200

    Exercise 4.6

--- part4/blogs-api/utils/list_helper.js
+++ part4/blogs-api/utils/list_helper.js
@@ -9,9 +9,28 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
     return  blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return undefined;
+  
+    const authorCount = {};
+    blogs.forEach(blog => {
+      authorCount[blog.author] = (authorCount[blog.author] || 0) + 1;
+    });
+      
+    const mostBlogsAuthor = Object.keys(authorCount).reduce((a, b) => 
+      authorCount[a] > authorCount[b] ? a : b
+    );
+  
+    return {
+      author: mostBlogsAuthor,
+      blogs: authorCount[mostBlogsAuthor]
+    };
+  };
   
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
